9358bb590eb67ccf9e66e8571d4df50ff5ba465a
Anna <annachristinethomas@gmail.com>
2021-09-16 12:49:12 -0600

    ok i am now understand overloading operators and the rule of three that if I have any destructor, copy constructor, or operator=, I need to implement all three to guarantee that each object is created and destroyed properly

--- overloadingPractice.cpp
+++ overloadingPractice.cpp
@@ -1,10 +1,12 @@
 #include <cmath>
-#include <string>
+//#include <string> don't include, only need cstring, which takes char * as parameters
+//because i'm writting a class for strings, I won't use a library that can't take char * as parameteres
+
 #include <iostream>
 #include <stdio.h>
 #include <stdint.h>
 #include <fstream>
-#include <cstring>
+#include <cstring> //aka <string.h>
 
 class MyString {
  
@@ -18,13 +20,17 @@ class MyString {
 
         //the operator << is a standalone function, so it needs the friend keyword to access private members
         //"It will define the function as a non-member function of the namespace surrounding the class it appears in" 
-        //so could you write friend in private technically?
+        //so could you write friend in private technically? //idk experiment u sussy
         friend std::ostream & operator << ( std::ostream & out, const MyString & rhs); 
         //use friend keyword to let the class and function know that whenever this function is used it is allowed to access the private members of this class
         
-        //these operator [] is a method, thus they don't need the friend keyword to access private members
+        //the operator [] is a method, thus they don't need the friend keyword to access private members
         char operator [](int index) const; //indexing into a string, so the return type will be a character //righthand side -->check notes for explanation
         char & operator [](int index ); //lefthand side -->check notes for explanation 
+
+        //the operator = is a unary operator, meaning it takes in one parameter 
+        //unary operator take one argument/parameter //you can use argument/parameter interechangeably 
+        MyString operator= (const MyString& aString);
 
         MyString(); //the default constructor
         MyString( const char * ); //another constructor whose paramater is a char array
@@ -69,20 +75,61 @@ char MyString::char operator[] ( int index ) const
     return data[index]; 
 }
 
+MyString& MyString::operator= (const MyString& otherString) //can return something that is a reference to a type
+{
+    // Guard against self assignment
+    if (this == &otherString){ //"this" in this context is a pointer that points at what the unary, overloaded operator "=" is being used on //what's on the lefthand side
+        return *this; //dereference "this" and return it
+
+    }
+
+        this-> data = new char[otherString.getLength()]; //the pointer of the char* temp is now pointing at "otherString.length's value" "spot" on the heap /* can have it point at any number of spots on the heap, if you do, these spots will be consecutive and avoid memory fragmentation */
+        //did this in preparation of allocating appropriate space for this-> data to store otherString.data since the unary operator = assigns the value of something on the righthand side to the variable on the lefthandside
+        //temp is my buffer //A buffer is temporary storage of data that is on its way to other media or storage of data
+        //the buffer will not have the same address (on the heap //because I used new, it will be the heap's address) as otherString, since we don't want to change otherString (it's a constant)
+        //thus we give it a different spot on the heap
+
+        //temp.getLength() = otherString.getLength(); //make the buffer's length match that of otherString //doesn't work because i can't assign a variable to a temporary
+        
+        this-> length = otherString.getLength(); //beause otherString is outside my class MyString::, i can't access its private members directly, hence usage of getLength()
+        
+        for(int a = 0; a < this-> length; a++){ //cycles through every index of data
+
+            this-> data[a] = otherString.getData(a); //assign all the values at temp[a] the values of otherString[a]
+            //these are private though so write a get for obtaining the value of something at index a 
+
+        }   
+
+     //a buffer is useful when doing a swap of some sort, otherwise it's extraneous
+     //Since I overloaded the operator = to also copy, I don't need a buffer. 
+     //my copy constructor can just simply use this overloaded operator in it if I want to replace the original object of type MyString with another object of type MyString
+     //or I can call this whever I want to copy an object of type MyString that replaces the original object of type MyString
+     //I can make a copy constructor that uses this overloaded operator in it, and that also preserves the original object of type MyString    
+ 
+    }
+
+    return *this; //can only allocate a pointer into a reference //look up
+    //return a dereferenced pointer to "this" , which is variable type MyString& 
+}
+
+
+//default constructor
 MyString::MyString() 
 { 
 
     data = new char[1]; //tells it to allocate one "spot" on the heap for storing the values of char array data (char * data)
-    data[0] = 0; //tells it that the char array data indexed at zero is null as 0 is the null terminating character 
-    //reads 0 as a char, and on the ascii table, dec 0 is NULL //whereas dec 48 is the number 0
-    length = 0;
+    data[0] = 0; //tells it that the char array data indexed at zero is null as 0 is the "null terminating" character 
+    //reads 0 as a char since the type of data is char *, and on the ascii table, dec 0 is "NULL" //whereas dec 48 is the number 0
+
+    length = 0; //initialize length to 0 //not read as a char because length is type int
 }
 
 MyString::MyString( const char* words )
 {
 
-
-    length = strlen( words );
+    //Defined in header <cstring> //std::size_t strlen( const char* str );
+    //length() can only take in a string as a parameter, but strlen can take in a const char* aka char array as a parameter
+    length = strlen( words ); // "Returns the length of the given byte string, that is, the number of characters in a character array whose first element is pointed to by str up to and not including the first null character." 
     data = new char [length ];
 
     for( int i = 0; i < length; i++){
